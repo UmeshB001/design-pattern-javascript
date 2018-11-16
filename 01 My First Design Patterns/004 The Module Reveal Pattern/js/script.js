c1d92bff40ea5dcfb4d31330818f4e0fb13a34cf
unknown <umeshbombe001@gmail.com>
2018-11-16 12:28:13 +0530

    Module reveal Pattern

---
+++ 01 My First Design Patterns/004 The Module Reveal Pattern/js/script.js
@@ -0,0 +1,60 @@
+//See how we treat private and public members differently
+//Ensure private members can't intract wih public ones
+//Add complexity to our core structure 
+
+(function(){
+    var chatModule = (function(){
+        var _leadself = 'Me: ',
+        _leadcomputer = "PC: ",
+        _aSaid = ["This is a Cyber Chat"],
+        _msgYes = "Yes, that's a great idea.",
+        _msgNo = "No, that must be a mistake.",
+        _aSassyStuff = ["Like mold on books, grow myths on history.",
+                        "She moved like a poem and smiled like a sphinx.",
+                        "As long as we don’t die, this is gonna be one hell of a story.",
+                        "She laughed, and the desert sang.",
+                        "You’ve got about as much charm as a dead slug."];
+    
+        function _echo(msg){
+            _aSaid.push("<div>"+msg+"</div>");
+    
+            var aSaidLenght = _aSaid.length,
+                start = Math.max(aSaidLenght - 6,0);
+            out ="";
+            for(var i=start; i<aSaidLenght;i++){
+                out += _aSaid[i];
+            }
+            $('.advert').html(out);
+            $('#talk span').text(msg);
+        }
+    
+        function talk (msg){
+            _echo(_leadself + msg);
+        }
+    
+        function replayYesNo(){
+                        var msg = Math.random()>.6 ? _msgYes : _msgNo;
+                        _echo(_leadcomputer + msg);
+        }
+    
+        function saySassyStuff(){
+                        var msg = _aSassyStuff[Math.floor(Math.random()*_aSassyStuff.length)];
+                        _echo(_leadcomputer + msg);
+        }
+    
+        return{
+            talk : talk,
+            replayYesNo : replayYesNo,
+            saySassyStuff : saySassyStuff		
+        };
+    })();
+    
+    
+    
+    $(document).ready(function(){
+    chatModule.talk("this is great");
+    chatModule.replayYesNo();
+    chatModule.saySassyStuff();
+    });
+    
+    })();
